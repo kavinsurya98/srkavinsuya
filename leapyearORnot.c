2731299b40135ed5a27ced7a035306e5bbf14a0f
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-01 19:38:30 +0530

    Create leapyearORnot.c

---
+++ leapyearORnot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter the year :");
+scanf("%d",&i);
+if(i%4==0)
+{
+printf("leap year");
+}
+else
+{
+printf("not a leap year");
+}
+return 0
+}
