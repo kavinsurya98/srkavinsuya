1fdad2847a1148278215ad8609ad087d6bf73693
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-01 13:53:40 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter the number");
+scanf("%d",&i);
+if(i>0)
+{
+printf("postive");
+}
+else if(i<0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+getch()
+}
