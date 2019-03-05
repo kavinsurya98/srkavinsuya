56f58593b40e8c3aa6cef5ba4b57d167354f5c6e
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 05:07:10 +0530

    Create countstring.c

---
+++ countstring.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+	char a[100];
+	int n,i,count=0;
+	scanf(" %[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	 if(a[i]>='a'&&a[i]<='z'||a[i]>='A'&&a[i]<='Z')
+	 {
+	     count++;
+	 }
+	}
+	printf("%d",count);
+	return 0;
+}
