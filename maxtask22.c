7ed862111ccc186ce08da48176aa373882ebe0ab
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 04:38:41 +0530

    Create maxtask22.c

---
+++ maxtask22.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,m,i,gcd;
+	scanf("%d %d",&n,&m);
+	
+	for(i=2;i<=n&&i<=m;i++)
+	{
+              if(n%i==0&&m%i==0)
+              gcd=i;
+	}
+	printf("%d %d",gcd,gcd);
+	return 0;
+}
