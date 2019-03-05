b5e913cfd3a993460955e5b9b61a1060cbe1ac2e
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 05:09:37 +0530

    Create task32.c

---
+++ task32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+#include <string.h>
+ 
+void main()
+{
+    char s[200];
+    int c=0,i;
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("%d\n", c + 1);
+}
