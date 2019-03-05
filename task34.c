d3592f8544a439ea83b37815e5f82180739c2175
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 05:17:21 +0530

    Create task34.c

---
+++ task34.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main() 
+{
+	char a[1000];
+	int n,i,count=1;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]=='.'&& a[i+1]!='\0')
+	    {
+	        count++;
+	    }
+	}
+	printf("%d",count);
+	return 0;
+}
