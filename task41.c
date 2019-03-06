a80025cb1e3fbc0bbfbe0b38b930e25808d9f91b
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 05:40:15 +0530

    Create task41.c

---
+++ task41.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    char a[100]="sampath";
+    printf("how many time ")
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%s\n",a);
+    }
+}
