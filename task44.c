a7fd69ff13a43346e8941f66aa7da15d693a4556
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-06 05:47:15 +0530

    Create task44.c

---
+++ task44.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n;
+	scanf("%d",&n);
+    if(n>=1 && n<=10)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+	return 0;
