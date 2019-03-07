95a111e76bc4a870f39e8bbadcb27b9e0e87c206
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-07 06:34:42 +0530

    Create task61.c

---
+++ task61.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+	char a[100];
+	int n,i,num;
+	scanf("%s",a);
+	scanf("%d",&num);
+	n=strlen(a);
+    for(i=0;i<num;i++)
+    {
+        printf("%c",a[i]);
+    }
+	return 0;
+}
