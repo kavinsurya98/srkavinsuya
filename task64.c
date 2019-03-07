2bcdf6bac0efbac0729f7263cefd046106e219ba
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-07 06:41:26 +0530

    Create task64.c

---
+++ task64.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int n,m,sum=0;
+    scanf("%d %d",&n,&m);
+    sum=n+m;
+    if(sum%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+	return 0;
+}
