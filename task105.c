4d94d839b6dbc2a9a0d08cac0741255124c6a4a1
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-09 13:10:15 +0530

    Create task105.c

---
+++ task105.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num,i,n=65;
+    printf("enter number");
+    scanf("%d\t",&num);
+ for(i=1;i<num;i++)
+ {
+  n+=1;
+ }
+ printf("%c",n);
+   return 0;
+}
