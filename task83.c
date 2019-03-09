ec2048d177606ee58acb1e3eccb527701181f73a
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-09 12:06:04 +0530

    Create task83.c

---
+++ task83.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	int a,b,c;
+	int i;
+
+		scanf("%d %d",&a,&b);
+		if(i%2==0)
+		c=a/b;
+		else
+		c=a%b;
+	
+	
+		printf("\n%d",c);
+	
+	return 0;
+}
