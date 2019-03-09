0845c87012e03aff0a6c06cba121707a70f1d4b6
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-09 11:30:07 +0530

    Create task76.c

---
+++ task76.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<>conio.h
+void main()
+{
+ int n, i, count = 0;
+
+ scanf("%d",&n);
+
+ for(i=0;i<=n/2;i++)
+ {
+ if(n % i == 0 )
+  {
+   count = 1;
+   break;
+  }
+
+ }
+
+ if(count == 0)
+  printf("no");
+ else
+  printf("yes");
+ 
+ return 0;
+}
