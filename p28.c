4f6092a45154bddf47e9cb0afe3ae983e126a3c9
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-21 19:11:27 +0530

    Create p28.c

---
+++ p28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char b[50];
+    int i,j=0;
+    scanf("%[^\n]s",b);
+    for(i=0;b[i]!='\0';i++)
+    {
+        if(b[i]==' ')
+        {
+            b[i]='$';
+        }
+    }
+    for(i=0;b[i]!='\0';i++)
+    {
+        if(b[i]!='$') 
+        {
+            printf("%c",b[i]);
+        }
+    }
+    return 0;
+}
