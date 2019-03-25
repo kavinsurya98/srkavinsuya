0fed2e6e47456d41b76763305efa7dff98fc0a3e
kavinsurya98 <48118860+kavinsurya98@users.noreply.github.com>
2019-03-25 15:24:09 +0530

    Create pl26.c

---
+++ pl26.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+#include<string.h>
+
+int main(void) 
+{
+	char str[20];
+	int i;
+	scanf("%[^\n]s",&str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			str[i]='$';
+			break;
+		}
+	}
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			str[i]='+';
+		}
+	}
+	for(i=0;str[i]!='\0';i++)
+	{
+	if(str[i]=='$')
+	{
+		str[i]=' ';
+	}
+	if(str[i]!='+')
+	{
+	printf("%c",str[i]);
+	}
+	}
+	return 0;
+}
